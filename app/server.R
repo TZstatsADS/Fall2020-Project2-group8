c2f08434d2dcf0e41fadaab9302c2ef18adfbac5
Vikkiyyy <66074346+Vikkiyyy@users.noreply.github.com>
2020-10-06 20:52:21 -0400

    line plot

--- app/server.R
+++ app/server.R
@@ -18,7 +18,6 @@ library(tibble)
 
 shinyServer(function(input,output, session){
   load('../output/states_complete.RData')
-  
   #map --------------------------------------------------------------------------------------------------------
   
   #map end --------------------------------------------------------------------------------------------------------
@@ -27,6 +26,22 @@ shinyServer(function(input,output, session){
   #report --------------------------------------------------------------------------------------------------------
   
   #input$state_dropdown and input$policy_dropdown will give the values that the user inputted for the plot
+  d <- reactive({
+    filtered <- 
+      states_complete %>%
+      filter(State == input$state_dropdown)
+  }) 
+  # Line Plot
+  output$plot=renderPlot({
+    ggplot(d(), aes(x=Date, y=Incident_Rate,color=factor(get(input$policy_dropdown)))) +
+      geom_line(size=2) + 
+      theme_bw() +
+      xlab("Time") +
+      ylab("Incident Rate") +
+      ggtitle("Incident Rate Over Time")+
+      scale_colour_manual(values = c("plum1", "plum2", "plum3","plum4","mediumorchid4"))+
+      labs(color="Choosen Policy")
+    })
   
   #report end --------------------------------------------------------------------------------------------------------
   
--- app/ui.R
+++ app/ui.R
@@ -39,10 +39,14 @@ dashboardPage(
     
     #report --------------------------------------------------------------------------------------------------------
     tabItem(tabName = "Report",
+            fluidRow(column(12,
+                            h3("Interactive Dashboard on State and Policy"),
+                            "The following line plots show how the incident rate changes overtime and with certain policies being enforced")),
             selectInput(inputId="state_dropdown",label='Select State',
                         choices=unique(states_complete$State)),
             selectInput(inputId='policy_dropdown',label='Select Policy',
-                        choices=colnames(states_complete)[16:58][!endsWith(colnames(states_complete)[16:60],'Notes') & !endsWith(colnames(states_complete)[16:60],'Flag')][1:17])
+                        choices=colnames(states_complete)[16:58][!endsWith(colnames(states_complete)[16:60],'Notes') & !endsWith(colnames(states_complete)[16:60],'Flag')][1:17]),
+            plotOutput("plot")
     )
     #report end --------------------------------------------------------------------------------------------------------
   )
