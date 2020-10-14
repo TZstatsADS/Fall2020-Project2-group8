7d7c9808665deb6c2402a198b37a0b2ad0fa0984
rohan-uppuluri <rku2101@columbia.edu>
2020-10-14 12:42:55 -0700

    ui changes

--- app/ui.R
+++ app/ui.R
@@ -42,7 +42,7 @@ dashboardPage(
                 box(width = 15, title = "Introduction", status = "primary",
                     solidHeader = TRUE, h3("U.S. Covid-19 Policy Tracker"),
                     h4("By Zihan Chen, Xujie Ma, Rohan Uppuluri & Jiaqi Yuan"),
-                    h5("Covid-19 outbreaks have affected every region in the world. However, the magnitude of the impact has varied, as some regions have been successful in limiting the spread of the disease. There are many explanations of why some places have fewer cases than others. One of them is that the government policy response."),
+                    h5("Covid-19 outbreaks have affected every region in the world. However, the magnitude of the impact has varied, as some regions have been successful in limiting the spread of the disease. There are many explanations of why some places have fewer cases than others. One of them is the government policy response."),
                     h5("In this project, we built a policy tracker for policy makers in the U.S. to explore the interaction of state government policy response and Covid-19 statistics overtime (from 01/22/20 to 10/12/20). In particular, we want to see how some key Covid-19 statistics (confirmed cases, death, infection rate, mortality rate, positive test rate, hospitalization rate) change overtime and how the state governments are publishing corresponding containment and closure policies, health system policies, and economic policies."))),
               fluidRow(box(width = 15, title = "User Group", status = "primary",
                            solidHeader = TRUE, h3("Why Did We Develop this App?"),
