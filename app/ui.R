56bef7e5b3f59eb93827f49b9924a4d52721187b
rohan-uppuluri <rku2101@columbia.edu>
2020-10-13 21:44:44 -0700

    fix typo

--- app/ui.R
+++ app/ui.R
@@ -49,7 +49,7 @@ dashboardPage(
                     h5("In this project, we built a policy tracker to explore the interaction of state government policy response and Covid-19 statistics overtime (from 01/22/20 to 10/12/20). In particular, we want to see how some key Covid-19 statistics (confirmed cases, death, infection rate, mortality rate, positive test rate, hospitalization rate) change overtime and how the state governments are publishing corresponding containment and closure policies, health system policies, and economic policies."))),
               fluidRow(box(width = 15, title = "User Group", status = "primary",
                            solidHeader = TRUE, h3("Why Did We Develop this App?"),
-                           h5("Policy responses to Cover-19 are complex, context-specific and rapidly changing. Documenting the policies and the stringency can help policy makers to understand and assess government responses to Covid-19 over time. "),
+                           h5("Policy responses to Covid-19 are complex, context-specific and rapidly changing. Documenting the policies and the stringency can help policy makers to understand and assess government responses to Covid-19 over time. "),
                            h5("Our project provides the interactive plot of state government policy and Covid-19 statistics, and it could help to answer questions such as:"),
                            tags$div(tags$ul(
                              tags$li("1. How did policy stringency change with the evolving situation?"),
